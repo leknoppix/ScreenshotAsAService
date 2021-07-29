182c15e4f9e5842b3a3f947f9eb00f19e134188a
Cañadas Pascal <pascal@mirandais.fr>
2021-07-29 15:16:04 +0200

    Ajout d'un header Recherche sur chargement js de adsense et facebook erroné Recherche désactivation RGPD

--- server.js
+++ server.js
@@ -3,26 +3,24 @@ const { unlink } = require('fs');
 const path = require('path');
 const puppeteer = require("puppeteer");
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 app = express();
 app.get("/png/:url/:name", async (req, res) => {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
+    const browser = await puppeteer.launch({ headless: true, args: [
+      '--no-sandbox',
+      '--icognito',
+      '--user-agent=Mozilla/5.0 (Windows NT 10.0: Win64; x64) AppleWebKit/537.36 (WHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    ] })
     const page = await browser.newPage()
-    await page.goto(req.params.url, {waitUntil: ['load', 'networkidle0', 'domcontentloaded'] })
-    await page.click(".cc_btn.cc_btn_accept_all")
-    await page.waitForTimeout()
+    page.setDefaultNavigationTimeout(0);
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0: Win64; x64) AppleWebKit/537.36 (WHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36')
+    await page.goto(req.params.url, {waitUntil: ['load'], devtools: true })
     await page.setViewport({ width: 1280, height: 2000 })
-    const dest = path.join(__dirname, 'screenshot/' + req.params.name + '.png');
     image = await page.screenshot({fullPage: true})
-    await page.close()
-    await browser.close()
-    res.contentType('image/png');
-    res.send(image);
+    page.close()
+    browser.close()
+    res.contentType('image/png')
+    res.send(image)
 });
+
 app.listen(8888, () => console.log('Server Started'));
 module.exports = app;
