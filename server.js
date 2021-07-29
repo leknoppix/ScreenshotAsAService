9f69234bd027b34f73c57c6767575427188e72f3
Cañadas Pascal <pascal@mirandais.fr>
2021-07-29 22:19:17 +0200

    Mise en place d'une extension chrome Cookies rgpd

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express');
-const { unlink } = require('fs');
-const path = require('path');
+const fs = require('fs');
 const puppeteer = require("puppeteer");
+const path = require('path');
 
 app = express();
+
 app.get("/png/:url/:name", async (req, res) => {
-    const browser = await puppeteer.launch({ headless: true, args: [
-      '--no-sandbox',
-      '--icognito',
-      '--user-agent=Mozilla/5.0 (Windows NT 10.0: Win64; x64) AppleWebKit/537.36 (WHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-    ] })
-    const page = await browser.newPage()
-    page.setDefaultNavigationTimeout(0);
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0: Win64; x64) AppleWebKit/537.36 (WHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36')
-    await page.goto(req.params.url, {waitUntil: ['load'], devtools: true })
-    await page.setViewport({ width: 1280, height: 2000 })
-    image = await page.screenshot({fullPage: true})
-    page.close()
-    browser.close()
-    res.contentType('image/png')
-    res.send(image)
+  const pathToExtension = path.join(__dirname, 'extension/fihnjjcciajhdojfnbdddfaoknhalnja/3.3.1_0');
+  const browser = await puppeteer.launch({ headless: false, args: [
+    '--no-sandbox',
+    '--icognito',
+    '--user-agent=Mozilla/5.0 (Windows NT 10.0: Win64; x64) AppleWebKit/537.36 (WHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+    `--disable-extensions-except=${pathToExtension}`,
+    `--load-extension=${pathToExtension}`,
+  ] });
+  const page = await browser.newPage()
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(req.params.url, {waitUntil: 'networkidle2' })
+  await page.setViewport({ width: 1280, height: 720 })
+  image = await page.screenshot({fullPage: true})
+  page.close()
+  browser.close()
+  res.contentType('image/png')
+  res.send(image)
 });
 
 app.listen(8888, () => console.log('Server Started'));
